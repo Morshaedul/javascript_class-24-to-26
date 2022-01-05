07d946ea5ee3af3a1ca458373be3af675634c4bb
Morshaedul <morshaedulanirbanit@gmail.com>
2022-01-05 09:23:19 +0600

    javascript tutorial 24 to 27

---
+++ js/index.js
@@ -0,0 +1,42 @@
+// var mi=[10,20,50,30,40,50]
+// for(i=0; i<=5; i++){
+//   console.log(mi[i])
+// }
+
+//array loop use:
+// var num=[10,20,30,40,50,60]
+// for(i=0; i<=5; i++){
+//   console.log(num[i])
+// }
+
+// array loop use in prompt
+// var num=[10,20,30,40,50,60]
+// for(var i=0; i<=5; i++){
+//     num[i]=parseInt(prompt())
+// }
+// var sum=0;
+// for(i=0;i<=5;i++){
+//     document.write(" "+num[i]+" </br>")
+//   sum=sum+num[i];
+// }
+//   document.write("total value is :" +sum)
+
+//3 lenth ber korar jonno program
+
+// var text="Bangladesh"
+// len=text.length
+// document.write(len)
+
+// 5 string theke sthan sorano
+// parlam na,.
+
+// 6 akti string ar sudhumattro sorborno print korun .
+
+// Array number theke sum ber kora.
+var num=[10, 20, 30, 40, 50, 60]
+sum=0;
+for (i=0; i<6; i++){
+    document.write(" "+num[i] +"<br>")
+  sum=sum+num[i]
+}
+document.write(" The sum value is: " +sum)
